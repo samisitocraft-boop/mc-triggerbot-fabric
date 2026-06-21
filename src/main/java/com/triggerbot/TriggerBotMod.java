0351ae8971b81faf2f942060b77a175cf6149099
samisitocraft-boop <samisitocraft@gmail.com>
2026-06-21 00:08:23 -0400

    Simplify TriggerBotMod to remove slf4j dependency

--- src/main/java/com/triggerbot/TriggerBotMod.java
+++ src/main/java/com/triggerbot/TriggerBotMod.java
@@ -1,15 +1,12 @@
 package com.triggerbot;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TriggerBotMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("mc-triggerbot-fabric");
 	public static final String MOD_ID = "mc-triggerbot-fabric";
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("TriggerBot Mod initialized for Minecraft 1.20.1");
+		System.out.println("TriggerBot Mod initialized for Minecraft 1.20.1");
 	}
 }
